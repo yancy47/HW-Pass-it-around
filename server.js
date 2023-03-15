8e19653b4c5536ea0b3323d2cf8dd215dc79ee95
yancy47 <yancym47@gmail.com>
2023-03-15 13:39:56 -0500

    update

--- server.js
+++ server.js
@@ -8,11 +8,21 @@ const PORT = 8080
 
 // Create a "root" route 
 app.get('/', (req, res) => {
-    res.send('<h1>99 Bottles of tea on the wall</h1>')
+    res.send(
+        `<h1>99 Bottles of tea on the wall</h1>
+        <a href = '/98'> take one down, pass it around </a>`
+    ) 
 })
 
-app.get('/home', (req, res) => {
+app.get('/:number_of_bottles', (req, res) => {
     res.send('<h1>Home Page!!</h1>')
+    let bottles = req.params.number_of_bottles
+
+    if (bottles != 0) {
+        res.send(`<h1>${bottles} Bottles of tea on the wall</h1><a href='/${bottles - 1}'>take one down, pass it around</a>`)
+    } else {
+        res.send(`<h1>${bottles} Bottles of tea on the wall</h1><a href='/'>Start Over. Get more tea</a>`)
+    }
 })
 
 // Tell the app to listen on port 8080 for HTTP requests from clients
